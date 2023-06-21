d1f42e634c8d0c54242aa35f3529baa47addf2f5
bltaln <135560739+bltaln@users.noreply.github.com>
2023-06-21 14:52:46 +0200

    Create gen_pass.sh

---
+++ gen_pass.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Afișează mesajul de deschidere
+echo "Programul a fost pornit."
+echo "Script dezvoltat de www.linecode.ro"
+
+# Funcție pentru generarea parolei
+generate_password() {
+  # Definește setul de caractere pentru generarea parolei
+  characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-"
+  
+  # Lungimea parolei dorite
+  password_length=8
+  
+  # Generează parola
+  password=$(openssl rand -base64 48 | tr -dc "$characters" | head -c "$password_length")
+  
+  # Afișează parola generată
+  echo "Parola generată: $password"
+}
+
+# Generează și afișează o parolă
+generate_password
+
+# Poți adăuga orice alte acțiuni dorești să fie realizate în continuare în scriptul tău.
+
